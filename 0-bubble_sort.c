8934651d8e05cd39610a6be46712f0b67d2734a4
Groinkb <you@example.com>
2023-09-21 14:58:51 +0200

    bubble sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,27 @@
-updated
+#include "sort.h"
+
+/**
+ * insertion_sort - function that sorts elements in an array using
+ * insertion sort technique
+ * @array: array
+ * @size: size of the array
+ */
+void insertion_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int key;
+
+	for (i = 1; i < size; i++)
+	{
+		key = array[i];
+		j = i - 1;
+
+		while (j >= 0 && array[j] > key)
+		{
+			array[j + 1] = array[j];
+			j = j - 1;
+		}
+		array[j + 1] = key;
+		print_array(array, size);
+	}
+}
