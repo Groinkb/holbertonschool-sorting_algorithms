1a7fd2c195a3674a5072320963366b0f1fcca997
Groinkb <benjamin@mossu.ch>
2023-09-22 10:31:57 +0200

    betty style updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,32 +1,33 @@
 #include "sort.h"
+
 /**
- * selection_sort - Tri par sélection
- * @array: tableau à trier
- * @size: taille du tableau
+ * selection_sort - Trie un tableau d'entiers par tri par sélection.
+ * @array: Le tableau à trier.
+ * @size: La taille du tableau.
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_idx;
+	int tmp;
+
 	if (array == NULL || size < 2)
 		return;
 
-	size_t i, j, min_index;
-	int temp;
-
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
+		min_idx = i;
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < array[min_idx])
+				min_idx = j;
 		}
 
-		if (min_index != i)
+		if (min_idx != i)
 		{
-			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
 			print_array(array, size);
 		}
 	}
